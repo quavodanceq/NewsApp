5ef8ba93ba000a7e8838ce5deb89b5bfd223a658
quavodanceq <kuat.oralbekov1011@mail.ru>
2023-08-23 23:27:16 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Куат Оралбеков on 23.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
